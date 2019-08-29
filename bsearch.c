a67ae8acf16393a2e2fa71c83002c37e174453c0
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-08-29 10:56:46 +0530

    Update bsearch.c

--- bsearch.c
+++ bsearch.c
@@ -3,16 +3,10 @@
 void search(int a[],int lb,int ub,int x)
 {
     int mid;
-    if(lb==ub)
+    if(lb<=ub)
     {
-        if(a[lb]==x)
-            printf("Element found at position : %d \n" , lb+1);
-        else
-            printf("Element not found ! \n");
-    }
 
-    else
-    {
+
         mid=(lb+ub)/2;
         if(a[mid]==x)
             printf("Element found at position : %d \n" ,mid+1);
@@ -21,14 +15,19 @@ void search(int a[],int lb,int ub,int x)
         else
             search(a,lb,mid-1,x);
     }
+    else
+    {
+        printf(" Element not found ! ");
+    }
 }
+
 int main()
 {
     int n,a[100],i,x,ub,lb,mid;
     printf("Yash Saxena 170111049\n\n\n");
     printf("Enter length : ");
     scanf("%d",&n);
-    lb=0;ub=n;
+    lb=0;ub=n-1;
     printf("Enter array elements : ");
     for(i=0;i<n;i++)
     {
